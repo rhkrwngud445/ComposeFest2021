f26db72af3141e52bce44a60ca12f62d8b309c8b
joohyeong <rhkrwngud445@naver.com>
2021-11-15 22:27:07 +0900

    Title : 텍스트 요소 추가 Body : setContent 블록은 구성 가능한 함수를 호출하는 활동의 레이아웃 정의 구성 가능한 함수는 다른 구성 가능한 함수에서만 호출가능 JetPack Compose는 Kotlin 컴파일러 플러그인을 사용하여 구성 가능한 함수를 앱의 UI 요소를 반환

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -58,127 +58,128 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicsCodelabTheme {
-                MyApp()
-            }
+            Text("Hello world!")
+//            BasicsCodelabTheme {
+//                MyApp()
+//            }
         }
     }
 }
 
-@Composable
-private fun MyApp() {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-
-    if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-    } else {
-        Greetings()
-    }
-}
-
-@Composable
-private fun OnboardingScreen(onContinueClicked: () -> Unit) {
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue")
-            }
-        }
-    }
-}
-
-@Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
-        }
-    }
-}
-
-@Composable
-private fun Greeting(name: String) {
-    Card(
-        backgroundColor = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        CardContent(name)
-    }
-}
-
-@Composable
-private fun CardContent(name: String) {
-    var expanded by remember { mutableStateOf(false) }
-
-    Row(
-        modifier = Modifier
-            .padding(12.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                    stiffness = Spring.StiffnessLow
-                )
-            )
-    ) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(12.dp)
-        ) {
-            Text(text = "Hello, ")
-            Text(
-                text = name,
-                style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                )
-            )
-            if (expanded) {
-                Text(
-                    text = ("Composem ipsum color sit lazy, " +
-                        "padding theme elit, sed do bouncy. ").repeat(4),
-                )
-            }
-        }
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(
-                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
-                contentDescription = if (expanded) {
-                    stringResource(R.string.show_less)
-                } else {
-                    stringResource(R.string.show_more)
-                }
-
-            )
-        }
-    }
-}
-
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greetings()
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    BasicsCodelabTheme {
-        OnboardingScreen(onContinueClicked = {})
-    }
-}
+//@Composable
+//private fun MyApp() {
+//    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+//
+//    if (shouldShowOnboarding) {
+//        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+//    } else {
+//        Greetings()
+//    }
+//}
+//
+//@Composable
+//private fun OnboardingScreen(onContinueClicked: () -> Unit) {
+//    Surface {
+//        Column(
+//            modifier = Modifier.fillMaxSize(),
+//            verticalArrangement = Arrangement.Center,
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            Text("Welcome to the Basics Codelab!")
+//            Button(
+//                modifier = Modifier.padding(vertical = 24.dp),
+//                onClick = onContinueClicked
+//            ) {
+//                Text("Continue")
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+//    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+//        items(items = names) { name ->
+//            Greeting(name = name)
+//        }
+//    }
+//}
+//
+//@Composable
+//private fun Greeting(name: String) {
+//    Card(
+//        backgroundColor = MaterialTheme.colors.primary,
+//        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+//    ) {
+//        CardContent(name)
+//    }
+//}
+//
+//@Composable
+//private fun CardContent(name: String) {
+//    var expanded by remember { mutableStateOf(false) }
+//
+//    Row(
+//        modifier = Modifier
+//            .padding(12.dp)
+//            .animateContentSize(
+//                animationSpec = spring(
+//                    dampingRatio = Spring.DampingRatioMediumBouncy,
+//                    stiffness = Spring.StiffnessLow
+//                )
+//            )
+//    ) {
+//        Column(
+//            modifier = Modifier
+//                .weight(1f)
+//                .padding(12.dp)
+//        ) {
+//            Text(text = "Hello, ")
+//            Text(
+//                text = name,
+//                style = MaterialTheme.typography.h4.copy(
+//                    fontWeight = FontWeight.ExtraBold
+//                )
+//            )
+//            if (expanded) {
+//                Text(
+//                    text = ("Composem ipsum color sit lazy, " +
+//                        "padding theme elit, sed do bouncy. ").repeat(4),
+//                )
+//            }
+//        }
+//        IconButton(onClick = { expanded = !expanded }) {
+//            Icon(
+//                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+//                contentDescription = if (expanded) {
+//                    stringResource(R.string.show_less)
+//                } else {
+//                    stringResource(R.string.show_more)
+//                }
+//
+//            )
+//        }
+//    }
+//}
+//
+//@Preview(
+//    showBackground = true,
+//    widthDp = 320,
+//    uiMode = UI_MODE_NIGHT_YES,
+//    name = "DefaultPreviewDark"
+//)
+//@Preview(showBackground = true, widthDp = 320)
+//@Composable
+//fun DefaultPreview() {
+//    BasicsCodelabTheme {
+//        Greetings()
+//    }
+//}
+//
+//@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+//@Composable
+//fun OnboardingPreview() {
+//    BasicsCodelabTheme {
+//        OnboardingScreen(onContinueClicked = {})
+//    }
+//}
