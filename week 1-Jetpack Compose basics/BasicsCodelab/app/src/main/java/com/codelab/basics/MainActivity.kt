41c2024e0fb55deb9d73e4067307dd62f15eb82b
joohyeong <rhkrwngud445@naver.com>
2021-11-15 22:44:01 +0900

    Title : 레이아웃 - 여러 텍스트 추가 Body : PreviewMessagecard 함수에서 author과 body부분이 겹쳐서 표현된다 그래서 Column 함수를 사용하자

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -58,24 +58,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Android")
+            MessageCard(Message("Android","Jeytpack Compose"))
 //            BasicsCodelabTheme {
 //                MyApp()
 //            }
         }
     }
 }
+
+data class Message(val author : String, val body : String)
+
 @Composable
-fun MessageCard(name : String){
-    Text(text = "Hello $name")
+fun MessageCard(msg : Message){
+    Text(text = msg.author)
+    Text(text = msg.body)
 }
 
 @Preview
 @Composable
-fun DefaultPreview(){
-    BasicsCodelabTheme {
-        MessageCard("Android")
-    }
+fun PreviewMessageCard(){
+    MessageCard(msg = Message("Colleague","Hey, take a look at Jetpack Compose, it's great!"))
 }
 
 //@Composable
